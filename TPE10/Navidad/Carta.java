f69a9afbb45afeca868e8c3215822ad81707817d
Andy <76767534+andyian98@users.noreply.github.com>
2023-11-22 15:52:00 -0300

    Add files via upload

---
+++ TPE10/Navidad/Carta.java
@@ -0,0 +1,10 @@
+public class Carta{
+    Persona remitente;
+    ArrayList<String> listaDeRegalos=new ArrayList<>
+
+    public Carta(Persona remitente, listaDeRegalos<>){
+        this.remitente=remitente;
+        //
+    }    
+
+}
---
+++ TPE10/Navidad/Persona.java
@@ -0,0 +1,15 @@
+public class Persona{
+    int DNI;
+
+    public Persona(String DNI){
+        this.DNI=DNI;
+
+    }
+
+
+    
+    void depositar(Carta c, Buzon b){
+        buzon.getCartas<>.add(Carta c);
+    }
+
+}

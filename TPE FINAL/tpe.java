4600b8b7eba8d64bd0abdf113295d6fc6d67489e
Andy <76767534+andyian98@users.noreply.github.com>
2024-01-30 14:25:01 -0300

    Add files via upload

---
+++ TPE FINAL/tpe.java
@@ -0,0 +1,244 @@
+
+//CLASE MAZO 
+ 
+public class Mazo{
+    ArrayList <Carta> cartas;
+ 
+    public Mazo crearMazo(){ 
+     Mazo mazo=new Mazo();
+    
+    return mazo;     
+    
+    } }
+
+public Carta{ 
+    private String personaje; 
+    private ArrayList <Atributo> atributos;     
+    
+     
+    public Carta(){     
+        this.personaje=personaje;
+            
+    } 
+ 
+
+} 
+//CLASE ATRIBUTO
+public class Atributo {
+    private String nombre;
+    private int valor;
+
+    public Atributo(String nombre, int valor) {
+        this.nombre = nombre;
+        this.valor = valor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+}
+ 
+//CLASE JUEGO 
+public class Juego{
+    private Jugador j1; 
+    private Jugador j2; 
+ 
+    public Juego(Jugador j1, Jugador j2){
+             
+     
+    }
+     
+    public void repartirCartas(){ 
+    
+     
+     
+    
+    } 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+}
+ 
+ 
+public class Jugador{
+    private Carta cartas<>; 
+    private String nombre;
+
+ 
+ 
+
+} 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
+ 
+ 
+
